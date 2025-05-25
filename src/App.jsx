0342f01a532d986cf36afc2b0f3f50eb44086e5c
Vybhav Podala <podalavybhav@gmail.com>
2025-05-25 14:25:43 +0530

    adding manage team link in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 import LandingPage from "./pages/LandingPage";
+import { CogIcon } from "./icons/CogIcon";
 
 function App() {
   return (
     <div>
-      <header className="py-2 mt-2 text-white bg-blue-500 rounded-xl">
-        <h1 className="text-xl font-bold text-center rounded-lg">
-          Release Tracker
-        </h1>
+      <header className="flex items-center justify-between px-4 py-2 mt-2 text-white bg-blue-500 rounded-xl">
+        <h1 className="mx-auto text-xl font-bold">Release Tracker</h1>
+        <NavLink
+          to="/manage-team"
+          className="flex items-center justify-end text-white hover:text-yellow-300 hover:underline"
+        >
+          <CogIcon className="w-6 h-6" />
+          <span>Manage Team</span>
+        </NavLink>
       </header>
       <main className="mx-16">
         <LandingPage />
